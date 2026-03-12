278089f3e551a0d8de927b6fe59b0be2631855f4
clover-cn <admin@jiyiy.com>
2026-03-12 17:59:28 +0800

    修复弹窗报错

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        // Element Plus 下拉框触发的 ResizeObserver 无害警告，过滤不弹
+        runtimeErrors: (error) => {
+          if (error.message === 'ResizeObserver loop completed with undelivered notifications.') {
+            return false;
+          }
+          return true;
+        }
+      }
+    }
+  }
 })
